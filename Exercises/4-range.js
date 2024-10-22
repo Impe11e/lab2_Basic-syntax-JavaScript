baf5b9c1eb2a5de0109391a7e9fe8ff65eefeb6f
ariia horunzha <1mpellee@gmail.com>
2024-10-22 11:45:59 +0300

    commit changes in lab2_js

---
+++ Exercises/4-range.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Implement function `range(start: number, end: number): array` returning
+// array with all numbers from the range [15, 30] including endpoints
+
+const range = (start, end) => {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+};
+
+module.exports = { range };
